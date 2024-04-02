de43fe6d9637f8a78f231b0a9e492a7f2efa05e2
CrisAlex <cads.14alex@outlook.com>
2024-04-02 17:26:35 -0500

    Add Persistence Installers

---
+++ CleanArchitecture.Persistence/PersistenceInstallers.cs
@@ -0,0 +1,13 @@
+﻿using CleanArchitecture.Application.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CleanArchitecture.Persistence
+{
+    public static class PersistenceInstallers
+    {
+        public static void AddPersistenceServices(this IServiceCollection services)
+        {
+            services.AddScoped<IDatabaseService, DatabaseService>();
+        }
+    }
+}
